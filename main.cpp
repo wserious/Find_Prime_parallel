64eb24c1dd0bf32df577df6e11526d9644d35adb
wserious <948436185@qq.com>
2017-06-19 02:01:22 +0800

    change to parallel [1.0]

--- main.cpp
+++ main.cpp
@@ -1,19 +1,27 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include "mingw.thread.h"
+
 using namespace std;
 
-void primer(int m);
-
-void primer(int m)
+void primer(int m, int n)
 {
     int count = 0;
     int j,k,i;
     int min,max;
 
+    if(m >= n) {
+        max = m;
+        min = n;
+    }else {
+        max = n;
+        min = m;
+    }
+
     vector<int> primers;
 
-    for(i = 1 ;i <= m;i = i + 2)
+    for(i = min ;i <= max;i = i + 2)
     {
         k = sqrt(i);
         for(j = 2;j <= k;j++)
@@ -28,21 +36,26 @@ void primer(int m)
         }
     }
     cout << count <<endl;
-    for( i = 0; i < count; i++)
+    /*for( i = 0; i < count; i++)
     {
         cout << primers[i] ;
         if(i < count - 1)
             cout << " ";
         else
             cout << endl;
-    }
+    }*/
 
 }
 int main()
 {
-    int x, y;
-    cout << "please input number:\n";
-    cin >> x;
-    primer(x);
+    int x, y, z, w;
+    cout << "input four numbers:\n";
+    cin >> x >> y;
+    cin >> z >> w;
+//    primer(x, y);
+    std::thread t1(primer, x, y);
+    std::thread t2(primer, z, w);
+    t1.join();
+    t2.join();
     return 0;
 }
