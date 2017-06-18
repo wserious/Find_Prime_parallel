0c5c169ed8d6e5a9c6199b695de58b7b1b83a38a
wserious <948436185@qq.com>
2017-06-18 23:51:41 +0800

    test change

--- main.cpp
+++ main.cpp
@@ -1,8 +1,48 @@
 #include <iostream>
-#include <cstdio>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    printf("newline");
+void primer(int m);
+
+void primer(int m)
+{
+    int count = 0;
+    int j,k,i;
+    int min,max;
+
+    vector<int> primers;
+
+    for(i = 1 ;i <= m;i = i + 2)
+    {
+        k = sqrt(i);
+        for(j = 2;j <= k;j++)
+        {
+            if(i % j ==0)break;
+        }
+        if(j >= k + 1)
+        {
+            count++;
+            primers.push_back(i);
+
+        }
+    }
+    cout << count <<endl;
+    for( i = 0; i < count; i++)
+    {
+        cout << primers[i] ;
+        if(i < count - 1)
+            cout << " ";
+        else
+            cout << endl;
+    }
+
+}
+int main()
+{
+    int x, y;
+    cout << "please input number:\n";
+    cin >> x;
+    primer(x);
     return 0;
 }
