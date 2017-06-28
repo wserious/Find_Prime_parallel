7abccf783d85ab6bd2b2361c348a25e70fa2e568
wserious <948436185@qq.com>
2017-06-28 15:34:09 +0800

    todo: debug the parallel part

--- main.cpp
+++ main.cpp
@@ -6,50 +6,54 @@
 #include <cmath>
 
 using namespace std;
+bool goToNext;
 
 void primer(int m);
 
 void primer(int m) {
     int count = 0;
-    int j,k,i;
-    int min,max;
+    int j, k, i;
+    int min, max;
 
     vector<int> primers;
 #pragma omp parallel for
-    for(i = 1 ;i <= m;i += 2) {
+    for (i = 1; i <= m; i += 2) {
         k = sqrt(i);
-#pragma omp parallel for
-        for(j = 2;j <= k;j++) {
-            if(i % j ==0) {
-                break;
+        goToNext = false;
+        int j_result = 0;
+//#pragma omp parallel for
+        for (j = 2; j <= k; j++) {
+            if (goToNext) {
+                continue;
+            }
+            if (i % j == 0) {
+                j_result = j;
+                goToNext = true;
             }
         }
-        if(j >= k + 1) {
-            count++;
-//#pragma omp ordered
-            primers.push_back(i);
-        }
-    }
-    cout << count <<endl;
-   /* for( i = 0; i < count; i++)
-    {
-        cout << primers[i] ;
-        if(i < count - 1)
-            cout << " ";
-        else
-            cout << endl;
-    }*/
 
+        if (j_result != 0) {
+            j = j_result;
+        }
+            if (j >= k + 1) {
+                count++;
+                primers.push_back(i);
+
+            }
+    }
+
+    cout << count << endl;
 }
+
 int main() {
     long beginTime = clock();
     int x, y;
-    x = 10000000;
+    x = 1000;
     cout << x << endl;
 //    cout << "please input number:\n";
 //    cin >> x;
-#pragma omp parallel
-    {primer(x);}
+//#pragma omp parallel
+    { primer(x); }
     long endTime = clock();
     cout << "time is: " << endTime - beginTime << endl;
     return 0;
